f54c8f293e9066eac17639ece07ebbb183cfc811
Iván Fernández <ivan.fernandez.perea@gmail.com>
2013-01-12 15:48:19 +0100

    Do exercises chapter 5

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,6 +4,21 @@ describe "Static pages" do
 
   subject { page }
 
+  it "should have the right links on the layout" do
+    visit root_path
+    click_link "About"
+    should have_selector 'title', text: full_title('About Us')
+    click_link "Help"
+    should have_selector 'title', text: full_title('Help')
+    click_link "Contact"
+    should have_selector 'title', text: full_title('Contact')
+    click_link "Home"
+    click_link "Sign up now!"
+    should have_selector 'title', text: full_title('Sign up')
+    click_link "sample app"
+    should have_selector 'title', text: full_title('')
+  end
+
   describe "Home page" do
     before { visit root_path }
 
