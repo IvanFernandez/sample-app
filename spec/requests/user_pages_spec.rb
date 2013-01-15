e78755193130e3f85963526bca0b5d9c364d4d27
Iván Fernández <ivan.fernandez.perea@gmail.com>
2013-01-15 11:04:54 +0100

    Finish user edit, update, index, and destroy actions

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -78,26 +78,79 @@ describe "User pages" do
 
 	describe "edit" do
 		let(:user) { FactoryGirl.create(:user) }
-		before { visit edit_user_path(user) }
-	end
+		before do
+			sign_in user
+			visit edit_user_path(user)
+		end	end
 
-	describe "authorization" do
+		describe "authorization" do
 
-		describe "for non-signed-in users" do
-			let(:user) { FactoryGirl.create(:user) }
+			describe "for non-signed-in users" do
+				let(:user) { FactoryGirl.create(:user) }
 
-			describe "in the Users controller" do
+				describe "in the Users controller" do
 
-				describe "visiting the edit page" do
-					before { visit edit_user_path(user) }
-					it { should have_selector('title', text: 'Sign in') }
+					describe "visiting the edit page" do
+						before { visit edit_user_path(user) }
+						it { should have_selector('title', text: 'Sign in') }
+					end
+
+					describe "submitting to the update action" do
+						before { put user_path(user) }
+						specify { response.should redirect_to(signin_path) }
+					end
 				end
+			end
+		end
 
-				describe "submitting to the update action" do
-					before { put user_path(user) }
-					specify { response.should redirect_to(signin_path) }
+		describe "index" do
+			before do
+				sign_in FactoryGirl.create(:user)
+				FactoryGirl.create(:user, name: "Bob", email: "bob@example.com")
+				FactoryGirl.create(:user, name: "Ben", email: "ben@example.com")
+				visit users_path
+			end
+
+			it { should have_selector('title', text: 'All users') }
+			it { should have_selector('h1',    text: 'All users') }
+
+			it "should list each user" do
+				User.all.each do |user|
+					page.should have_selector('li', text: user.name)
+				end
+			end
+
+			describe "pagination" do
+
+				before(:all) { 30.times { FactoryGirl.create(:user) } }
+				after(:all)  { User.delete_all }
+
+				it { should have_selector('div.pagination') }
+
+				it "should list each user" do
+					User.paginate(page: 1).each do |user|
+						page.should have_selector('li', text: user.name)
+					end
+				end
+			end
+
+			describe "delete links" do
+
+				it { should_not have_link('delete') }
+
+				describe "as an admin user" do
+					let(:admin) { FactoryGirl.create(:admin) }
+					before do
+						sign_in admin
+						visit users_path
+					end
+
+					it { should have_link('delete', href: user_path(User.first)) }
+					it "should be able to delete another user" do
+						expect { click_link('delete') }.to change(User, :count).by(-1)
+					end
+					it { should_not have_link('delete', href: user_path(admin)) }
 				end
 			end
 		end
 	end
-end
